c587ca5c0e79933f99328eaa5e0468bda95d19f8
walle79 <41576302+walle79@users.noreply.github.com>
2022-12-18 20:02:15 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://cuongnobro.github.io/Baby/";
+            window.location = "https://walle79.github.io/heart-emotion/";
           },
         });
       }
