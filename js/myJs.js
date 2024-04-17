59fd54d1e744235ad89538ab042756ae94a8a17c
son h <41576302+walle79@users.noreply.github.com>
2024-04-17 22:05:47 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://walle79.github.io/heart-emotion/";
+            window.location = "https://walle79.github.io/my-flower/";
           },
         });
       }
