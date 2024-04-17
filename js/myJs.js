4425cf7c07d15e10c7d0f2940ea5ba641ef41563
son h <41576302+walle79@users.noreply.github.com>
2024-04-18 00:49:06 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -107,27 +107,27 @@ $(document).ready(function () {
 
   // show popup
   $("#yes").click(function () {
-    // var audio = new Audio("sound/tick.mp3");
-    // audio.play();
-    // Swal.fire({
-    //   title: textConfig.text7,
-    //   html: true,
-    //   width: 900,
-    //   padding: "3em",
-    //   html: "<input type='text' class='form-control' id='txtReason'  placeholder='Whyyy'>",
-    //   background: '#fff url("img/7720A0CE-FC26-49CB-9898-F983140C8918.jpeg")',
-    //   backdrop: `
-    //                 rgba(0,0,123,0.4)
-    //                 url("img/none.gif")
-    //                 left top
-    //                 no-repeat
-    //               `,
-    //   showCancelButton: false,
-    //   confirmButtonColor: "#3085d6",
-    //   cancelButtonColor: "#d33",
-    //   confirmButtonColor: "#fe8a71",
-    //   cancelButtonColor: "#f6cd61",
-    //   confirmButtonText: textConfig.text6,
+    var audio = new Audio("sound/tick.mp3");
+    audio.play();
+    Swal.fire({
+      title: textConfig.text7,
+      html: true,
+      width: 900,
+      padding: "3em",
+      html: "<input type='text' class='form-control' id='txtReason'  placeholder='Whyyy'>",
+      background: '#fff url("img/7720A0CE-FC26-49CB-9898-F983140C8918.jpeg")',
+      backdrop: `
+                    rgba(0,0,123,0.4)
+                    url("img/none.gif")
+                    left top
+                    no-repeat
+                  `,
+      showCancelButton: false,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonColor: "#fe8a71",
+      cancelButtonColor: "#f6cd61",
+      confirmButtonText: textConfig.text6,
     }).then((result) => {
       if (result.value) {
         Swal.fire({
